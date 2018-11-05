20baa1f876c2524ef790e7ffbbbf75b5e4679f0c
FolivoraDev <FolivoraDev@gmail.com>
2018-11-05 16:51:21 +0900

    user-upload file들을 저장할 때 mediastorage를 사용, S3 버킷에 저장

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -10,7 +10,7 @@ secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
 
 DATABASES = secrets['DATABASES']
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -10,3 +10,9 @@ secrets = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
 
 DATABASES = secrets['DATABASES']
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
