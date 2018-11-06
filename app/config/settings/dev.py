7e184fd14cd183ed9f81e23e9e8f1340d340f323
FolivoraDev <FolivoraDev@gmail.com>
2018-11-06 11:13:12 +0900

    StaticStorage 생성 적용

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -11,7 +11,7 @@ secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
 DATABASES = secrets['DATABASES']
 
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
