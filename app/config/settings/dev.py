a5fcba456b26deda38500418b4f3633b0dd16234
FolivoraDev <FolivoraDev@gmail.com>
2018-11-07 17:27:19 +0900

    r

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -11,34 +11,8 @@ secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
 DATABASES = secrets['DATABASES']
 
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
-STATICFILES_STORAGE = 'config.storages.StaticStorage'
+# STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
-
-LOG_DIR = os.path.join(ROOT_DIR, '.log')
-if not os.path.exists(LOG_DIR):
-    os.makedirs(LOG_DIR, exist_ok=True)
-
-LOGGING = {
-    'version': 1,
-    'handlers': {
-        'file_error': {
-            'class': 'logging.FileHandler',
-            'level': 'DEBUG',
-            'filename': os.path.join(LOG_DIR, '.log'),
-        },
-        'console': {
-            'class': 'logging.StreamHandler',
-            'level': 'INFO',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file_error', 'console'],
-            'level': 'INFO',
-            'propagate': True,
-        }
-    }
-}
