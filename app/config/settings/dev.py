22a189fdf833820f7f857d4f5c8f22406c458491
FolivoraDev <FolivoraDev@gmail.com>
2018-11-07 16:49:41 +0900

    q

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -16,3 +16,32 @@ STATICFILES_STORAGE = 'config.storages.StaticStorage'
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+
+LOG_DIR = os.path.join(ROOT_DIR, '.log')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'file_error': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'filename': os.path.join(LOG_DIR, '.log'),
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file_error', 'console'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
+
+DEBUG = False
+ALLOWED_HOSTS = ['abc']
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -15,3 +15,21 @@ DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'filename': os.path.join(ROOT_DIR, '.log'),
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    }
+}
